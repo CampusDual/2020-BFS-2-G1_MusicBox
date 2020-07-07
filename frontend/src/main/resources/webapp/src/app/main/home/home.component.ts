a817016ff32d98b037060eeb5d3f70bc6ff7dd36
sx! <mon.medialab@gmail.com>
2020-07-07 19:03:49 +0200

    (MBX-33 búsqueda por canción)

--- frontend/src/main/resources/webapp/src/app/main/home/home.component.ts
+++ frontend/src/main/resources/webapp/src/app/main/home/home.component.ts
@@ -1,118 +1,124 @@
-import { Component, OnInit, Output } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Component, OnInit, Output } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
-import { HomeService } from '../services/home.service';
-import { AppComponent } from 'app/app.component';
+import { HomeService } from '../services/home.service';
+import { AppComponent } from 'app/app.component';
 
 @Component({
-  selector: 'home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit {
 
-  radioSelectedValue = '';
-  inputSearchValue: any;
-  counter = 0;
-  columns: string[];
-  urlRoute: string;
-  startSearch = false;
-  result: any;
-  fieldsListSearch: string;
-  fieldListKey: string;
-  fieldName: any;
-  routeImage: string;
-  arrayRadioValues = [{'key': 'artist_name',
-    'value': 'Artist'}, {'key': 'disc_name',
-    'value': 'Disc'}, {'key': 'song_name',
-    'value': 'Song'}];
-  
-  @Output()
-  appComponet = new AppComponent();
+  radioSelectedValue = '';
+  inputSearchValue: any;
+  counter = 0;
+  columns: string[];
+  urlRoute: string;
+  startSearch = false;
+  result: any;
+  fieldsListSearch: string;
+  fieldListKey: string;
+  fieldName: any;
+  routeImage: string;
+  routerOption: string;
+  arrayRadioValues = [{'key': 'artist_name',
+    'value': 'Artist'}, {'key': 'disc_name',
+    'value': 'Disc'}, {'key': 'song_name',
+    'value': 'Song'}];
 
-  constructor(
-    private router: Router,
-    private actRoute: ActivatedRoute,
-    protected homeService: HomeService
-  ) {
-  }
+  
+  
+  
+  @Output()
+  appComponet = new AppComponent();
 
-  ngOnInit() {
-  }
+  constructor(
+    private router: Router,
+    private actRoute: ActivatedRoute,
+    protected homeService: HomeService
+  ) {
+  }
 
-  radioValue(value: any) {
-    console.log(value);
-    this.startSearch = true;
-    this.radioSelectedValue = value.value;
-    console.log(this.radioSelectedValue);    
+  ngOnInit() {
+  }
 
-    if (this.startSearch && this.inputSearchValue != '') {
-      this.homeService.searchItems(this.inputSearchValue, 
-        this.getColumns(this.radioSelectedValue), this.getUrlRoute(this.radioSelectedValue))
-      .subscribe(
-        res => this.result = res && res['data'] ? res['data'] :  []
-        );
-    }
-    return event;
-  }
+  radioValue(value: any) {
+    console.log(value);
+    this.startSearch = true;
+    this.radioSelectedValue = value.value;
+    console.log(this.radioSelectedValue);    
 
-  /* Valor por defecto del radio-button */
-  getValue() {
-    return 'artist_name';
-  }
-  
-  textValue(event: any) {
-    console.log(event.target.value.value);  
-    this.counter++;
-    this.inputSearchValue = event.target.value.value;
+    if (this.startSearch && this.inputSearchValue != '') {
+      this.homeService.searchItems(this.inputSearchValue, 
+        this.getColumns(this.radioSelectedValue), this.getUrlRoute(this.radioSelectedValue))
+      .subscribe(
+        res => this.result = res && res['data'] ? res['data'] :  []
+        );
+    }
+    return event;
+  }
 
-    if (this.startSearch && this.inputSearchValue.length >= 3) {
-      this.homeService.searchItems(this.inputSearchValue, this.getColumns(this.radioSelectedValue), this.getUrlRoute(this.radioSelectedValue))
-      .subscribe(
-        res => this.result = res && res['data'] ? res['data'] :  []
-        );
-    } else {
-      this.result = [];
-    }
-  }
+  /* Valor por defecto del radio-button */
+  getValue() {
+    return 'artist_name';
+  }
+  
+  textValue(event: any) {
+    console.log(event.target.value.value);  
+    this.counter++;
+    this.inputSearchValue = event.target.value.value;
 
-  /*comentario creado de prueba*/ 
-  /*comentario creado de prueba*/ 
-  navigate() {
-    this.router.navigate(['../', 'login'], { relativeTo: this.actRoute });
-  }
+    if (this.startSearch && this.inputSearchValue.length >= 3) {
+      this.homeService.searchItems(this.inputSearchValue, this.getColumns(this.radioSelectedValue), this.getUrlRoute(this.radioSelectedValue))
+      .subscribe(
+        res => this.result = res && res['data'] ? res['data'] :  []
+        );
+    } else {
+      this.result = [];
+    }
+  }
 
-  getColumns(radioValue: string) { 
-    if (radioValue === 'artist_name') {
-      this.columns = ['id_artist', 'artist_name', 'artist_bio', 'artist_photo'];
-      this.fieldsListSearch = 'id_artist;artist_name;artist_bio;artist_photo';
-      this.fieldListKey = 'id_artist';
-      this.fieldName = 'artist_name';
-      this.routeImage = this.appComponet.imageArtistRoute;
-    } else if (radioValue === 'disc_name') {
-      this.columns = ['id_disc', 'id_artist', 'disc_name', 'producer'];
-      this.fieldsListSearch = 'id_disc;id_artist;disc_name;producer';
-      this.fieldListKey = 'id_disc';
-      this.fieldName = 'disc_name';
-      this.routeImage = this.appComponet.imageDiscRoute;
-    } else if (radioValue === 'song_name') {
-      this.columns = ['id_song', 'id_gender', 'song_name', 'song_length'];
-      this.fieldsListSearch = 'id_song';'id_gender';'song_name';'song_length';
-      this.fieldListKey = 'id_song';
-      this.fieldName = 'song_name';
-      this.routeImage = this.appComponet.imageSongRoute;
-    }
-    return this.columns;
-    }
+  navigate() {
+    this.router.navigate(['../', 'login'], { relativeTo: this.actRoute });
+  }
 
-    getUrlRoute(radioValue: string) {
-      if (radioValue === 'artist_name') {
-        this.urlRoute = '/artists/artistsSearch';
-      } else if (radioValue === 'disc_name') {
-        this.urlRoute = '/discs/discSearch';
-      } else if (radioValue === 'song_name') {
-        this.urlRoute = '/songs/songSearch';
-      }
-      return this.urlRoute;
-      }
+  getColumns(radioValue: string) { 
+    if (radioValue === 'artist_name') {
+      this.columns = ['id_artist', 'artist_name', 'artist_bio', 'artist_photo'];
+      this.fieldsListSearch = 'id_artist;artist_name;artist_bio;artist_photo';
+      this.fieldListKey = 'id_artist';
+      this.fieldName = 'artist_name';
+      this.routerOption = '/main/artist';
+      console.log(this.routerOption);
+      this.routeImage = this.appComponet.imageArtistRoute;
+    } else if (radioValue === 'disc_name') {
+      this.columns = ['id_disc', 'id_artist', 'disc_name', 'producer'];
+      this.fieldsListSearch = 'id_disc;id_artist;disc_name;producer';
+      this.fieldListKey = 'id_disc';
+      this.fieldName = 'disc_name';
+      this.routerOption = '/main/disc';
+      this.routeImage = this.appComponet.imageDiscRoute;
+    } else if (radioValue === 'song_name') {
+      this.columns = ['id_song', 'id_gender', 'song_name', 'song_length'];
+      this.fieldsListSearch = 'id_song';'id_gender';'song_name';'song_length';
+      this.fieldListKey = 'id_song';
+      this.fieldName = 'song_name';
+      this.routerOption = '/main/song';
+      this.routeImage = this.appComponet.imageSongRoute;
+    }
+    return this.columns;
+    }
+
+    getUrlRoute(radioValue: string) {
+      if (radioValue === 'artist_name') {
+        this.urlRoute = '/artists/artistsSearch';
+      } else if (radioValue === 'disc_name') {
+        this.urlRoute = '/discs/discSearch';
+      } else if (radioValue === 'song_name') {
+        this.urlRoute = '/songs/songSearch';
+      }
+      return this.urlRoute;
+      }
 }
--- ws/src/main/java/com/ontimize/mbx/ws/core/rest/SongRestController.java
+++ ws/src/main/java/com/ontimize/mbx/ws/core/rest/SongRestController.java
@@ -1,8 +1,12 @@
 package com.ontimize.mbx.ws.core.rest;
 
+ 
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+ 
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
@@ -12,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+ 
+
 import com.ontimize.db.EntityResult;
 import com.ontimize.db.SQLStatementBuilder;
 import com.ontimize.jee.server.rest.ORestController;
@@ -19,38 +25,48 @@ import com.ontimize.mbx.api.core.service.ISongService;
 import com.ontimize.mbx.api.core.service.ITool;
 import com.ontimize.mbx.model.core.dao.SongDao;
 
+ 
+
 @RestController
 @RequestMapping("/songs")
 @ComponentScan(basePackageClasses = { com.ontimize.mbx.api.core.service.ISongService.class,
-									  com.ontimize.mbx.api.core.service.ITool.class })
+                                      com.ontimize.mbx.api.core.service.ITool.class })
 public class SongRestController extends ORestController<ISongService> {
 
-	@Autowired
-	private ISongService songSrv;
+ 
 
-	@Autowired
-	private ITool toolSrv;
+    @Autowired
+    private ISongService songSrv;
 
-	@Override()
-	public ISongService getService() {
-		return this.songSrv;
-	}
+ 
 
-	@RequestMapping(value = "/songSearch", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-	public EntityResult resultSearchLikeByName(@RequestBody Map<String, Object> req) {
-		try {
-			List<String> columns = (List<String>) req.get("columns");
-			Map<String, Object> filter = (Map<String, Object>) req.get("filter");
-			String song_name = (String) filter.get("search_name");
-			Map<String, Object> key = new HashMap<String, Object>();
-			key.put(SQLStatementBuilder.ExtendedSQLConditionValuesProcessor.EXPRESSION_KEY,
-					toolSrv.searchObjectByLikeName(SongDao.ATTR_NAME, song_name));
-			return songSrv.songQuery(key, columns);
-		} catch (Exception e) {
-			e.printStackTrace();
-			EntityResult res = new EntityResult();
-			res.setCode(EntityResult.OPERATION_WRONG);
-			return res;
-		}
-	}
+    @Autowired
+    private ITool toolSrv;
+
+ 
+
+    @Override()
+    public ISongService getService() {
+        return this.songSrv;
+    }
+
+ 
+
+    @RequestMapping(value = "/songSearch", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public EntityResult resultSearchLikeByName(@RequestBody Map<String, Object> req) {
+        try {
+            List<String> columns = (List<String>) req.get("columns");
+            Map<String, Object> filter = (Map<String, Object>) req.get("filter");
+            String song_name = (String) filter.get("search_name");
+            Map<String, Object> key = new HashMap<String, Object>();
+            key.put(SQLStatementBuilder.ExtendedSQLConditionValuesProcessor.EXPRESSION_KEY,
+                    toolSrv.searchObjectByLikeName(SongDao.ATTR_NAME, song_name));
+            return songSrv.songQuery(key, columns);
+        } catch (Exception e) {
+            e.printStackTrace();
+            EntityResult res = new EntityResult();
+            res.setCode(EntityResult.OPERATION_WRONG);
+            return res;
+        }
+    }
 }
